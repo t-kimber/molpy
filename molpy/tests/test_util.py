5bbde4ceeff5f5fefcbb5d2614144275b7f2c33f
Talia Kimbers <talia.kimber@gmail.com>
2020-02-12 13:59:15 +0100

    util: add test for success and failure

--- molpy/tests/test_util.py
+++ molpy/tests/test_util.py
@@ -1,5 +1,16 @@
+import pytest
 import molpy
 
-def test_distance():
+@pytest.mark.parametrize("point1, point2, bench", [
+	([0,1], [0,0], 1),
+	([0,0], [0,0], 0),
+	([2.236, 2.236], [0,0], 3.162)
+])
 
-	assert molpy.util.distance([0,1], [0,0]) == 1
+def test_distance(point1, point2, bench):
+
+	assert molpy.util.distance(point1, point2) == pytest.approx(bench, abs=1.e-3)
+
+def test_distance_failure():
+
+	assert molpy.util.distance([0], [3]) != 5
