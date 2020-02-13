33283966688ffe6497c7122c7e400033aa8ba4fa
Talia Kimbers <talia.kimber@gmail.com>
2020-02-13 09:57:52 +0100

    add reader

--- molpy/util.py
+++ molpy/util.py
@@ -1,22 +1,37 @@
 import numpy as np
 
+
+def read_xyz(filename):
+
+    with open(filename, "r") as handle:
+        data = handle.readlines()
+
+    data = data[2:]
+    data = [x.split() for x in data]
+    print(data)
+    symbols = [x[0] for x in data]
+    xyz = np.array([[float(y) for y in x[1:]] for x in data])
+
+    return {"symbols": np.array(symbols), "geometry": xyz}
+
+
 def distance(point1, point2):
-	"""
-	Calculate the euclidian distance between two points.
+    """
+    Calculate the euclidian distance between two points.
 
-	Parameters
-	----------
-	point1: array_like
-		The first point.
-	point2: array_like
-		The second point.
+    Parameters
+    ----------
+    point1: array_like
+        The first point.
+    point2: array_like
+        The second point.
 
-	Returns
-	-------
-	float
-		The distance between point1 and point 2.
-	"""
+    Returns
+    -------
+    float
+        The distance between point1 and point 2.
+    """
 
-	point1 = np.asarray(point1)
-	point2 = np.asarray(point2)
-	return np.linalg.norm(point1-point2)
+    point1 = np.asarray(point1)
+    point2 = np.asarray(point2)
+    return np.linalg.norm(point1 - point2)
